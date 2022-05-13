40eecfb038578dcfa5b8e8fa99924c619ad1ee5c
Julien Sayd <julien.sayd@outlook.fr>
2022-05-13 20:12:14 +0200

    2E VERSION CODE SUPER MARCHE

--- Article.java
+++ Article.java
@@ -1,22 +1,62 @@
 package Supermarket;
 
-
 public class Article {
 
 
-    public String nom;
-    public int qty;
-    public double prix, reduction;
+    private String nom;
+    private int qty;
+    private double prix, reduction;
 
 
-    public double getPrixTotal() {
-        return prix * qty;
+
+
+    public String getNom() {
+        return nom;
     }
 
-    public double getPrixTotalAvecReduc() {
-        return getPrixTotal() * reduction;
+    public void setNom(String nom) {
+        this.nom = nom;
     }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public double getReduction() {
+        return reduction;
+    }
+
+    public void setReduction(double reduction) {
+        this.reduction = reduction;
+    }
+
+    public Article(String nom, int qty, double prix, double reduction) {
+        this.nom = nom;
+        this.qty = qty;
+        this.prix = prix;
+        this.reduction = reduction;
+    }
+
+    public double getPrixTotal() {return prix * qty ;
+    }
+    public double getPrixtotalreduction(){return getPrixTotal() + prix * qty * reduction;
+
+    }
+
 }
+
 
 
 
--- Panier.java
+++ Panier.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Panier {
 
 
-     public  ArrayList<Article> articles ;
+     private  ArrayList<Article> articles ;
 
 
 
